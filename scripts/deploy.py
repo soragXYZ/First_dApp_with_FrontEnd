924ea543895729d5385ac8c6429e7322167f0809
sorag <sorag.eth@protonmail.com>
2021-10-26 16:14:42 +0200

    Front end

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,14 @@
 from scripts.helpful_scripts import get_account, get_contract
 from brownie import DappToken, TokenFarm, config, network
 from web3 import Web3
+import yaml
+import json
+import os
+import shutil
 
 KEPT_BALANCE = Web3.toWei(100,"ether")
 
-def deploy_token_farm_and_dapp_token():
+def deploy_token_farm_and_dapp_token(front_end_update=False):
     account = get_account()
     dapp_token = DappToken.deploy({"from": account})
     token_farm = TokenFarm.deploy(
@@ -23,6 +27,8 @@ def deploy_token_farm_and_dapp_token():
     }
 
     add_allowed_tokens( token_farm, dict_of_allowed_tokens, account)
+    if update_front_end:
+        update_front_end()
     return token_farm, dapp_token
 
 
@@ -35,7 +41,20 @@ def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
         set_tx.wait(1)
     return token_farm
 
+def update_front_end():
 
+    copy_folders_to_front_end("./build", "./front_end/src/chain-info")
+
+    with open("brownie-config.yaml", "r") as brownie_config:
+        config_dict = yaml.load(brownie_config, Loader=yaml.FullLoader)
+        with open("./front_end/src/brownie-config.json", "w") as brownie_config_json:
+            json.dump(config_dict, brownie_config_json)
+    print("Front end updated")
+
+def copy_folders_to_front_end(src, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    shutil.copytree(src, dest)
 
 def main():
-    deploy_token_farm_and_dapp_token()
+    deploy_token_farm_and_dapp_token(front_end_update=True)
